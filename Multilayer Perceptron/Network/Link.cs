8166aca68ad40b731e852230191f095607e26af5
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-10-29 14:50:56 +0300

    Added neuron and link.

---
+++ Multilayer Perceptron/Network/Link.cs
@@ -0,0 +1,9 @@
+﻿namespace MultilayerPerceptron.Network
+{
+    public class Link
+    {
+        public double Input { set; private get; }
+        public double Output => Input * Weight;
+        public double Weight { get; set; }
+    }
+}
--- Multilayer Perceptron/Network/Neuron.cs
+++ Multilayer Perceptron/Network/Neuron.cs
@@ -1,17 +1,53 @@
-﻿namespace MultilayerPerceptron.Network
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace MultilayerPerceptron.Network
 {
     public class Neuron
     {
-        private NeuronType type;
+        private readonly List<Link> inputLinks;
+        private readonly List<Link> outputLinks;
+        private double threshold;
 
-        public Neuron(NeuronType type)
+        public Neuron()
         {
-            this.type = type;
+            inputLinks = new List<Link>();
+            outputLinks = new List<Link>();
         }
-    }
+        
+        public enum LinkType {Input, Output}
 
-    public enum NeuronType
-    {
-        Input, Hidden, Output
+        public void AddLink(LinkType linkType, Link link)
+        {
+            switch (linkType)
+            {
+                case LinkType.Input:
+                    inputLinks.Add(link);
+                    break;
+                case LinkType.Output:
+                    outputLinks.Add(link);
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException();
+            }
+        }
+
+        public void Teach(double weightDelta, double threasholdDelta)
+        {
+            threshold += threasholdDelta;
+        }
+
+        public double GetValue()
+        {
+            double result = inputLinks.Sum(link => link.Output) + threshold;
+            return Activate(result);
+        }
+
+        private double Activate(double x)
+        {
+            return 1.0 / (1 + Math.Pow(Math.E, -x));
+        }
     }
 }
